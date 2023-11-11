2167e15d47757bdd4a62ca894421e4c27ba0ede9
Christian Chen <cchen9186@gmail.com>
2023-11-11 00:28:33 -0500

    Initial commit

---
+++ detector.py
@@ -0,0 +1,11 @@
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import ColorMode, Visualizer
+from detectron2 import model_zoo
+
+import cv2
+import numpy as np
+
+class Detector:
+    
